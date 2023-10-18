bf207e9c7a6ca30d9480db634be3b3147fca7d36
Pradarss <verma0025adarsh@gmail.com>
2023-10-18 15:05:18 +0530

    ..

---
+++ server.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const cors = require("cors");
