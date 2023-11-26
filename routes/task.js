1a910da821a3217bb25d5e3fbd6456e6ba46031b
Pradarss <verma0025adarsh@gmail.com>
2023-11-26 21:24:06 +0530

    ..

--- routes/task.js
+++ routes/task.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const Todo = require('../models/lists/todo');
+// const getCurrentTime = require('../time');
+
+function getCurrentTime(){
+  const date = new Date();
+  const options = { hour: '2-digit', minute: '2-digit', hour12: true };
+  return date.toLocaleTimeString('en-US', options);
+  
+  }
 
 router.post("/dashboard", (req,res)=>{
+  console.log("Received request body:", req.body);
     const { task, time } = req.body;
 
     const newTask = new Todo({
       task,
-      time,
+      time : getCurrentTime(),
     });
 
     console.log(newTask);
@@ -22,3 +31,5 @@ router.post("/dashboard", (req,res)=>{
       res.status(500).json({ error: 'Internal Server Error' });
     });
 })
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 // const session = require("express-session");
 // const passport = require("passport");
 const cors = require("cors");
+const taskroutes = require("./routes/task");
 
 const app = express();
 const port = 5000;
@@ -18,6 +19,7 @@ app.use(cors({
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use("/",taskroutes);
 
 mongoose.connect("mongodb://127.0.0.1:27017/trelloDB", {useNewUrlParser: true})
 .then(function(){
