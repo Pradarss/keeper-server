a4d80e8a4385466fde2b5486c15a6f9352c1cb6f
Sukriti <singhsukriti0@gmail.com>
2024-02-25 12:04:51 +0530

    .

--- routes/auth.js
+++ routes/auth.js
@@ -1,27 +1,18 @@
+const LocalStrategy = require('passport-local').Strategy; 
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-
-const Manager = require('../models/users/manager');
 const Employee = require('../models/users/employee');
+const Manager = require('../models/users/manager');
 
-passport.use('manager-local', Manager.createStrategy());
-passport.use('employee-local', Employee.createStrategy());
+// Configure Passport with Local Strategy for Employee
+passport.use(new LocalStrategy(Employee.authenticate()));
 
-passport.serializeUser(function(user, done) {
-  done(null, { id: user.id, type: user.constructor.modelName });
-});
+// Serialize and deserialize Employee user
+passport.serializeUser(Employee.serializeUser());
+passport.deserializeUser(Employee.deserializeUser());
 
-passport.deserializeUser(function(obj, done) {
-  if (obj.type === 'Manager') {
-    Manager.findById(obj.id, function(err, manager) {
-      done(err, manager);
-    });
-  } else if (obj.type === 'Employee') {
-    Employee.findById(obj.id, function(err, employee) {
-      done(err, employee);
-    });
-  } else {
-    done(new Error('Unknown model type'));
-  }
-});
+// Configure Passport with Local Strategy for Manager
+passport.use(new LocalStrategy(Manager.authenticate()));
 
+// Serialize and deserialize Manager user
+passport.serializeUser(Manager.serializeUser());
+passport.deserializeUser(Manager.deserializeUser());
