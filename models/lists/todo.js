227917f456fdce293b704e302f91946a15e955cc
Pradarss <verma0025adarsh@gmail.com>
2024-03-04 12:40:23 +0530

    bug fixed

--- models/lists/todo.js
+++ models/lists/todo.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 
 const todoSchema = mongoose.Schema({
-    task: String,
-    time: String,
+    task: {
+        type : String,
+        required :  true,
+    },
+    time: {
+        type : String,
+        required :  true,
+    },
     status: String,
     // manager_id: String,
 })
